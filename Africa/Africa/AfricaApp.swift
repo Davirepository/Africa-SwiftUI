97cb3bdc7562681134a78ce35c5639157d6dbef4
David Bolotaev <bolotaev-david@mail.ru>
2021-05-08 18:44:15 +0300

    Add some images, content, first prepare

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Давид on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+	var body: some Scene {
+		WindowGroup {
+			MainView()
+		}
+	}
+}
---
+++ Africa/Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Давид on 08.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		Text("Content")
+			.padding()
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
